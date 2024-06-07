3f183d938e74f1b1e646b2d1a352ee2c56a8be59
Stinamariya <stinamariya2006@gmail.com>
2024-06-07 13:31:31 +0530

    complete

---
+++ src/components/ViewBook.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+
+const ViewBook = () => {
+  return (
+    <div>
+        <div className="container">
+            <h1><center>VIEW BOOK</center></h1>
+            <div className="row">
+                <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                <table class="table"BORDER="3">
+  <thead>
+    <tr>
+      <th scope="col">TITLE</th>
+      <th scope="col">AUTHOR</th>
+      <th scope="col">PUBLISHED YEAR</th>
+      <th scope="col">PRICE</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Tom and jerry</td>
+      <td>Vasco</td>
+      <td>2001</td>
+      <td>200</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Tom and jerry</td>
+      <td>Vasco</td>
+      <td>2001</td>
+      <td>200</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Tom and jerry</td>
+      <td>Vasco</td>
+      <td>2001</td>
+      <td>200</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Tom and jerry</td>
+      <td>Vasco</td>
+      <td>2001</td>
+      <td>200</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Tom and jerry</td>
+      <td>Vasco</td>
+      <td>2001</td>
+      <td>200</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Tom and jerry</td>
+      <td>Vasco</td>
+      <td>2001</td>
+      <td>200</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Tom and jerry</td>
+      <td>Vasco</td>
+      <td>2001</td>
+      <td>200</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Tom and jerry</td>
+      <td>Vasco</td>
+      <td>2001</td>
+      <td>200</td>
+    </tr>
+  </tbody>
+</table>
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ViewBook
