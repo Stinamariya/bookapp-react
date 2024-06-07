13c152d1613ee88a0e026e2b6919c8d815da810d
Stinamariya <stinamariya2006@gmail.com>
2024-06-07 12:56:58 +0530

    complete

---
+++ src/components/SearchBook.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const SearchBook = () => {
+  return (
+    <div>
+        <div className="container">
+            <h1><center>SEARCH BOOK</center></h1>
+            <div className="row g-3">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label htmlFor="" className="form-label">BOOK TITLE</label>
+                    <input type="text" className="form-control" />
+                </div>
+                
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <center><button className="btn btn-success">Search</button></center>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SearchBook
